a29f0e340e36aac590b972c5d2078607312aeebd
Jesus Villalobos Jr <jesus.villalobosjr@gmail.com>
2023-08-15 17:12:16 -1000

    Add function to play one game of rock paper scissors

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,3 +5,27 @@ function getComputerChoice(){
     let choice = gameChoices[randomIndex];
     return choice;
 }
+
+function playRound(playerSelection,computerSelection){
+    playerSelection = formatWord(playerSelection);
+
+    if(!gameChoices.includes(playerSelection))
+        console.log("Invalid choice please try again.")
+
+    let playerIndex = gameChoices.indexOf(playerSelection);
+    let computerIndex = gameChoices.indexOf(computerSelection);
+
+    let result = (computerIndex === indexOfLoss(playerIndex)) ? `Computer Wins! ${gameChoices[computerIndex]} beats ${gameChoices[playerIndex]}` : (playerIndex === indexOfLoss(computerIndex)) ? `Player Wins! ${gameChoices[playerIndex]} beats ${gameChoices[computerIndex]}` : `It's a Tie!`;
+}
+
+function formatWord(word){
+    word = word.toLowerCase();
+    word = word.split("");
+    word[0] = word[0].toUpperCase();
+    word = word.join("");
+    return word;
+}
+
+function indexOfLoss(index){
+    return (index + 1) % gameChoices.length;
+}
